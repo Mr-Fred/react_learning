541dfab28828a1cf7f022368b0d3f2c34f68fc52
Fred Adeniyi <adewil1992@gmail.com>
2025-07-12 03:17:06 +0800

    fix: update conditional rendering for previous and next links in PokemonPage component

--- part11/src/PokemonPage.jsx
+++ part11/src/PokemonPage.jsx
@@ -31,9 +31,9 @@ const PokemonPage = ({ previous, next }) => {
   return (
     <>
       <div className="links">
-        {previous && <Link to={`/pokemon/${previous.name}`}>Previous</Link>}
+        {previous ? <Link to={`/pokemon/${previous.name}`}>Previous</Link> : <span></span>}
         <Link to="/">Home</Link>
-        {next && <Link to={`/pokemon/${previous.name}`}>Next</Link>}
+        {next ? <Link to={`/pokemon/${next.name}`}>Next</Link> : <span></span>}
       </div>
       <div className={`pokemon-page pokemon-type-${type.name}`}>
         <div className="pokemon-image" style={{ backgroundImage: `url(${pokemon.sprites.front_default})` }} />
@@ -60,6 +60,7 @@ const PokemonPage = ({ previous, next }) => {
     </>
   )
 }
+
 PokemonPage.propTypes = {
   previous: PropTypes.shape({
     name: PropTypes.string
